4811546519d7bbd77a291de3d0db1c9b31e6bacd
Jarek <jarek707@gmail.com>
2021-06-17 14:44:38 -0700

    added grid files

---
+++ summing-grid/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import SummingGrid from './SummingGrid';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SummingGrid />
+      </div>
+    );
+  }
+}
+
+export default App;
